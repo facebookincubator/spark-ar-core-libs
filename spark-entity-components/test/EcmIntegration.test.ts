67a0a73a6e6242750b93457395afaaef4d284b7e
Artem Goncharov <artom@meta.com>
2023-03-15 05:28:12 -0700

    Added integration test for ECM (only on ECM level)
    Summary:
    Added integration test for ECM (only on ECM level)
    
    This test uses real Manager, Entity and Component in order to test their behaviour in scope of ECM
    
    Differential Revision: D43839245
    
    fbshipit-source-id: dbf7903de1f6adf479f03c15a7ceda2500671ee7

---
+++ spark-entity-components/test/EcmIntegration.test.ts
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {expect, test, jest, beforeEach} from '@jest/globals';
+import {SceneEntity, SceneEntityState} from '../src/SceneEntity';
+import {SceneEntityManager} from '../src/SceneEntityManager';
+import {SceneEntityComponentState, SceneEntityComponent} from '../src/SceneEntityComponent';
+import {SceneEntityFrameUpdateListener} from '../src/SceneEntityFrameCallback';
+import Scene from 'Scene';
+
+jest.mock('../src/SceneEntityFrameCallback');
+
+class TestComponent extends SceneEntityComponent {
+  onFrame = jest.fn();
+  onEnable = jest.fn();
+  onCreate = jest.fn();
+  onDisable = jest.fn();
+  onStart = jest.fn();
+  onDestroy = jest.fn();
+}
+
+function getActiveSceneObject(identifier: string): any {
+  return {
+    identifier: identifier,
+    findByPath: jest.fn().mockImplementation(() => []),
+    hidden: {
+      pinLastValue: () => false,
+      monitor: () => ({
+        subscribe: jest.fn(),
+      }),
+    },
+  };
+}
+
+beforeEach(() => {
+  (SceneEntityManager['_instance'] as any) = null;
+});
+
+test('Run manager on visible SceneObjects with loadScene, then add components and when call OnFrame on manager - compoenents should get onFrame', async () => {
+  const subscribeMock = jest.fn();
+  SceneEntityFrameUpdateListener['instance'] = {
+    registerCallback: subscribeMock,
+  };
+
+  const childSceneObject1 = getActiveSceneObject('child1');
+  const childSceneObject2 = getActiveSceneObject('child2');
+
+  // Scene root returns two children
+  Scene.mockRoot = {
+    findByPath: jest.fn().mockImplementationOnce(() => [childSceneObject1, childSceneObject2]),
+  };
+
+  // run manager
+  await SceneEntityManager.run({loadSceneGraph: true});
+
+  const entity1 = SceneEntityManager.instance['_sceneEntities'].get('child1') as any;
+  const entity2 = SceneEntityManager.instance['_sceneEntities'].get('child2');
+
+  expect(entity1).toBeDefined();
+  expect(entity2).toBeDefined();
+  expect(SceneEntityManager.instance.getEntitySceneChildren('root').length).toBe(2);
+
+  // add components
+  const component1 = SceneEntityManager.instance
+    .getEntityById('child1')
+    .addComponentOfType(TestComponent);
+  const component2 = SceneEntityManager.instance
+    .getEntityById('child2')
+    .addComponentOfType(TestComponent);
+
+  // components created callbacks were called
+  expect(component1.onCreate).toBeCalledTimes(1);
+  expect(component2.onCreate).toBeCalledTimes(1);
+  expect(component1.onEnable).toBeCalledTimes(0);
+
+  // await for the next frame when all components async callbacks are finished
+  await new Promise(resolve => setTimeout(resolve, 1));
+
+  // all components should be enabled and started
+  expect(component1.onEnable).toBeCalledTimes(1);
+  expect(component1.onStart).toBeCalledTimes(1);
+  expect(component2.onEnable).toBeCalledTimes(1);
+  expect(component2.onStart).toBeCalledTimes(1);
+
+  // after onFrame callback come to Manager
+  SceneEntityManager.instance.onFrame();
+
+  // onFrame callbacks should come to all components
+  expect(component1.onFrame).toBeCalledTimes(1);
+  expect(component2.onFrame).toBeCalledTimes(1);
+
+  // entity of the first component become hidden
+  entity1['updateVisibility'](false);
+
+  // first component was disabled because of this
+  expect(component1.onDisable).toBeCalledTimes(1);
+
+  // and next time onFrame comes to Manager only second component will get onFrame
+  SceneEntityManager.instance.onFrame();
+  expect(component1.onFrame).toBeCalledTimes(1);
+  expect(component2.onFrame).toBeCalledTimes(2);
+
+  entity1['setActive'] = jest.fn();
+  // first entity got enabled and then destroyed
+  entity1['updateVisibility'](true);
+  expect(component1.onEnable).toBeCalledTimes(2);
+  entity1.destroy();
+
+  // first component was disabled and destroyed
+  expect(component1.onDisable).toBeCalledTimes(2);
+  expect(component1.onDestroy).toBeCalledTimes(1);
+
+  // entity set underlying scene object visibility to false
+  expect(entity1['setActive']).lastCalledWith(false);
+});
